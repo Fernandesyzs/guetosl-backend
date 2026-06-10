cfeb99df33c63d835f73ffb14c54d8f379894873
Fernandesyzs <menordasprata011@icloud.com>
2026-06-09 21:21:15 -0300

    spring boot configurado e conectado ao mysql

---
+++ src/main/java/com/guetosl/guetosl/GuetoslApplication.java
@@ -0,0 +1,13 @@
+package com.guetosl.guetosl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuetoslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuetoslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guetosl/guetosl/GuetoslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guetosl.guetosl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuetoslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
